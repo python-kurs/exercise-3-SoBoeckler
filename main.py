83c900827257a08406dcb909b7df3e72c1dc2414
SoBoeckler <33057744+SoBoeckler@users.noreply.github.com>
2019-05-27 15:15:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-# Exercise 3
+#Exercise 3
 from pathlib import Path
-# import functions from utils here
-
-data_dir = Path("data")
-output_dir = Path("solution")
-
+import os
+import utils
+    
 # 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
 
+data_dir = "C:/Users/Admin/Documents/GitHub/exercise-3-SoBoeckler/data"
+output_dir = data_dir + "solution/"
+
+
+creat_cars_path=Path(data_dir, "cars.txt")
 
 # 2. Read the text file [2P]
 
+cars=open(str(creat_cars_path), "r").read()
 
 # 3. Count the occurences of each item in the text file [2P]
 
+carlist=list(cars.split("\n"))#bessere listengestaltung(Wörtertrennung)
+count= word_count(carlist)
 
 # 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
 
+os.path.isdir(data_dir+"/solution") #if true the dir exists
+###Path('C:/Users/Admin/Documents/GitHub/exercise-3-SoBoeckler/data/solution').mkdir(parents=True, exist_ok=True) 
 
 # 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header): [2P]
+
 #    item, count
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
 #    ...
+with open(output_dir+"count.csv", 'w') as file:
+    for key in count.keys():
+        file.write("%s,%s\n"%(key,count[key]))
+
+
--- utils.py
+++ utils.py
@@ -2,3 +2,20 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+
+
+def word_count(cl:list):
+    """
+    counts how often a word exist (add everytime plus 1) and if there ist only one word it sets the count on one.
+    """
+    cars={}
+    for i in cl:
+        if i in cars.keys():
+            cars[i] += 1
+        else:
+            cars[i]=1
+    return cars
+
+
+
